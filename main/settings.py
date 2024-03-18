f9eb511cbd4424ffbfb3a03dea5a09028aff6e0d
abishekvp <abishek.vp13@gmail.com>
2024-03-18 20:07:30 +0530

    update minor

--- main/settings.py
+++ main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sk%m#p69^#2##-nvoppd(pf==wb0gg-rby^p--=(9)6k@ct7%)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 

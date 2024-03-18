ba100c2a13fcf8db69e9d534ccce9a3b06785ea9
abishekvp <abishek.vp13@gmail.com>
2024-03-18 23:51:29 +0530

    update5

--- main/settings.py
+++ main/settings.py
@@ -79,10 +79,16 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sql6692422',
+        'USER': 'sql6692422',
+        'PASSWORD': 'HpgLSr2IV4',
+        'HOST':'sql6.freesqldatabase.com',
+        'PORT':'3306',
     }
 }
 

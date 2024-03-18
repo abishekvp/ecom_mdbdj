3718f8ebe155ad293044edf574572004a3cc8dc5
abishekvp <abishek.vp13@gmail.com>
2024-03-18 21:34:30 +0530

    update

--- main/settings.py
+++ main/settings.py
@@ -121,11 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static','app'),
+    os.path.join(BASE_DIR, 'static','user_admin'),
+    os.path.join(BASE_DIR, 'static','per_user'),
+    os.path.join(BASE_DIR, 'static','plumber'),
+    os.path.join(BASE_DIR, 'static','prime'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

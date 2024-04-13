452447ae9cd6f26c054c6ac4d27d4e428f05fc65
Abishek VP <96417967+abishekvp@users.noreply.github.com>
2024-04-13 09:43:13 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,6 @@
-from django.db import models
+from django.contrib.auth.models import Group
+
+new_group, created = Group.objects.get_or_create(name='user_admin')
+new_group, created = Group.objects.get_or_create(name='per_user')
+new_group, created = Group.objects.get_or_create(name='plumber')
+new_group, created = Group.objects.get_or_create(name='prime')
